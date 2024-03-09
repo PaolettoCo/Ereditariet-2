0e1bf3051824837b759285f7218be77e4602c95a
Paolo Conti <152903837+PaolettoCo@users.noreply.github.com>
2024-03-09 18:32:55 +0100

    Create Rettangolo.java

---
+++ Rettangolo.java
@@ -0,0 +1,15 @@
+public class Rettangolo extends Forma {
+    private int lato1;
+    private int lato2;
+
+    public Rettangolo(int lato1, int lato2) {
+        this.lato1 = lato1;
+        this.lato2 = lato2;
+    }
+
+    @Override
+    public void calcolaArea() {
+        double area = lato1 * lato2;
+        System.out.println("L'area del rettangolo: " + area);
+    }
+}
